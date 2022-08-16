e07c0903a82f773e1c6d068809181b6d58df4c17
coco24 <1281299809@qq.com>
2022-08-16 21:31:59 +0800

    Remove exported symbol `some` and `every`

--- package.lisp
+++ package.lisp
@@ -1,2 +1,7 @@
 (uiop:define-package generic-cl-ext
   (:mix-reexport #:generic-cl-ext.collection #:generic-cl-ext.trivia #:generic-cl))
+
+(in-package #:generic-cl-ext)
+
+(unexport 'some)
+(unexport 'every)
